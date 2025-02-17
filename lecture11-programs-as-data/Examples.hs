caca86d75144be53c02308b35eeebcbcb388c073
rachellepena <rmp356@drexel.edu>
2025-02-17 14:40:38 -0500

    Add subtraction, negation, and division operations to expression evaluation

--- lecture11-programs-as-data/Examples.hs
+++ lecture11-programs-as-data/Examples.hs
@@ -18,6 +18,9 @@ data Exp = Var Var
          | Const Double
          | Add Exp Exp
          | Mul Exp Exp
+         | Sub Exp Exp
+         | Neg Exp
+         | Div Exp Exp
   deriving (Show)
 
 -- | Evaluate expressions
@@ -28,6 +31,9 @@ eval env (Var v)     = case lookup v env of
 eval _   (Const n)   = n
 eval env (Add e1 e2) = eval env e1 + eval env e2
 eval env (Mul e1 e2) = eval env e1 * eval env e2
+eval env (Sub e1 e2) = eval env e1 - eval env e2
+eval env (Neg e) = - (eval env e)
+eval env (Div e1 e2) = eval env e1 / eval env e2
 
 -- Compute derivative of an expressions with respect to a variable
 deriv :: Exp -> Var -> Exp
@@ -38,6 +44,11 @@ deriv (Const _) _   = Const 0
 deriv (Add e1 e2) x = Add (deriv e1 x) (deriv e2 x)
 deriv (Mul e1 e2) x = Add (Mul e1 (deriv e2 x))
                           (Mul (deriv e1 x) e2)
+deriv (Sub e1 e2) x = Sub (deriv e1 x) (deriv e2 x)
+deriv (Neg e) x = Mul (Const (-1)) (deriv e x)
+deriv (Div e1 e2) x = Div (Sub (Mul e2 (deriv e1 x))
+                          (Mul e1 (deriv e2 x)))
+                          (Mul e2 e2)
 
 -- | "Compile" an expression into a Haskell function
 compile :: Exp -> Double -> Double
